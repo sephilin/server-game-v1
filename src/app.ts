a1ba8ce7416cc92f0fcfca5ae2dfeecd1f2fea13
Menezes Vitor (Externe) <RMEV008@PROD.MRQ>
2020-01-06 15:34:14 -0500

    socket pronto para conexao e registro de eventos

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,53 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express from 'express';
+import * as http from 'http';
+import SocketIO from 'socket.io';
+import { Server } from 'http';
 
+const port = process.env.PORT || 3000
 
 class App {
-  public express: Application;
+  private _app: express.Application;
+  private server: Server;
+  private io: SocketIO.Server;
 
   constructor() {
-    this.express = express();
-
-    const router = express.Router()
-    router.get('/', (req: Request, res: Response, next: NextFunction) => {
-      res.json({
-        message: 'Hello Everybody'
-      });
-    })
-    this.express.use('/', router);
+    this._app = express();
+    this.server = http.createServer(this._app);
+    this.initSocket();
   }
+
+  private initSocket(): void {
+    this.io = SocketIO(this.server);
+  }
+
+  public listen(): void {
+    // server listening on our defined port
+    this.server.listen(port, () => {
+      console.log('Running server on port %s', port);
+    });
+
+
+    //socket events
+    this.io.on('connection', this.connection);
+  }
+
+  connection = (socket: SocketIO.Socket) => {
+    console.log('connected with ID:', socket.id);
+    //connection is up, let's add a simple simple event
+    socket.on('message', this.onMessage);
+    socket.on('disconnect', this.disconnect);
+  }
+
+  disconnect = (reason: string) => {
+    console.log('disconnected:', reason);    
+  }
+
+  onMessage = (message: string) => {
+    //log the received message and send it back to the client
+    console.log(message);
+    this.io.emit('broadcast', `Hello, you sent -> ${message}`);
+  }
+
 }
 
-export default new App().express;
+export default new App();
